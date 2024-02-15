8a014aeb487ad1541693670d60f59c2e3ab4cce0
Julia Ortiz <contato@juliaortiz.com.br>
2024-02-15 17:09:50 -0300

    try publishing again

--- examples/with-next-and-wagmi/src/wagmi.ts
+++
@@ -1,7 +0,0 @@
-import { createDAppStoreClient } from "@evmos/DAppStore-sdk";
-import { createWalletClient, custom } from "viem";
-export const DAppStoreClient = createDAppStoreClient();
-export const provider = DAppStoreClient.provider;
-export const client = createWalletClient({
-  transport: custom(provider),
-});
