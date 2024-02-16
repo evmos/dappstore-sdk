86a142cbdbd5f409c43dab049f2a95a2ecc66515
Julia Ortiz <contato@juliaortiz.com.br>
2024-02-16 17:57:45 -0300

    hardcode template in a string because of bun's macro bug

--- packages/dappstore-cli/src/init/get-template.ts
+++ packages/dappstore-cli/src/init/get-template.ts
@@ -1,17 +1,26 @@
-import { readFile } from "fs/promises";
-import path from "path";
-import { fileURLToPath } from "url";
+const TEMPLATE = `
+import { createDAppStoreClient } from "@evmos/dappstore-sdk";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-export const readTemplate = async () => {
-  try {
-    console.log(process.cwd());
-    return await readFile(
-      path.join(__dirname, `./templates/dappstore-client.ts`),
-      "utf-8"
-    );
-  } catch (e) {
-    console.error("Template not found");
-    process.exit(1);
-  }
+export const dappstore = createDAppStoreClient();
+
+/**
+ * EIP-1193 provider
+ */
+export const provider = dappstore.provider;
+`;
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export const readTemplate = () => {
+  // try {
+  //   return await readFile(
+  //     path.join(__dirname, `./templates/dappstore-client.ts`),
+  //     "utf-8"
+  //   );
+  // } catch (e) {
+  //   console.error("Template not found");
+  //   process.exit(1);
+  // }
+
+  // TODO: I was actually reading this file with a Bun macro but it's not working on github CI
+  // let's try again in the future
+  return TEMPLATE;
 };
